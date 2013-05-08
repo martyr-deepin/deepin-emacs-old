0f9bdf3226bf1ea246010adca7e7e89c14d9c1ff
Andy Stewart <lazycat.manatee@gmail.com>
2013-05-08 21:56:43 +0800

    Just change default font size to avoid can't startup if system haven't some font.

--- lazycat-lisp/Site-Lisp/Configure/LazyCatFont.el
+++ lazycat-lisp/Site-Lisp/Configure/LazyCatFont.el
@@ -1,12 +1,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 字体设置 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(set-face-attribute 'default nil :height 130)
+
 ;; (defvar emacs-font-name "DejaVu Sans YuanTi Mono"
-(defvar emacs-font-name "文泉驿等宽微米黑"
-  "The font name of English.")
-(defvar emacs-font-size 12
-  "The default font size.")
-(if (display-grayscale-p)
-    (progn
-      (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
-      (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))))
+;; (defvar emacs-font-name "文泉驿等宽微米黑")
+;; (defvar emacs-font-name "XHei Mono.Ubuntu")
+;; (defvar emacs-font-size 12
+;;   "The default font size.")
+;; (if (display-grayscale-p)
+;;     (progn
+;;       (set-frame-font (format "%s-%s" (eval emacs-font-name) (eval emacs-font-size)))
+;;       (set-fontset-font (frame-parameter nil 'font) 'unicode (eval emacs-font-name))))
 
 (provide 'LazyCatFont)
